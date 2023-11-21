73e8c0ea34fdfbd2ac197732e7ff5bdd3f4939c1
orhunakkan <orhunakkan@outlook.com>
2023-11-21 11:24:54 -0600

    function challenge

---
+++ 07-functions-boxes-of-milk/index.js
@@ -0,0 +1,28 @@
+// Calculate the number of boxes that your robot can get
+
+function numberOfBottles (moneyAmount){
+    return Math.floor(moneyAmount / 1.5);
+}
+
+function getMilk(moneyAmount) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("Go into store and buy " + numberOfBottles(moneyAmount) + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk(20);
