5e537e86716d74dac281b6837f0fc9ea90c85e6a
orhunakkan <orhunakkan@outlook.com>
2023-11-21 08:37:01 -0600

    dog age calculation

---
+++ 05-dog-age-calculation/index.js
@@ -0,0 +1,11 @@
+// Dog age to human age formula
+
+// humanAge = (dogAge-2) * 4 + 21
+// humanAge - 21 = ((dogAge-2) * 4)
+// (humanAge - 13) / 4 = dogAge
+
+var myAge = 33;
+var myDogAge;
+
+myDogAge = (33 - 13) / 4;
+console.log(myDogAge);
